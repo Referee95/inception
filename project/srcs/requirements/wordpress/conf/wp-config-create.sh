92cc46b2514b4402743abb2717b4e768126b0a27
referee95 <ysfm7khalil@gmail.com>
2024-07-05 12:22:55 -0400

    wordpress script

--- project/srcs/requirements/wordpress/conf/wp-config-create.sh
+++ project/srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
+wp core download --path=/var/www/html --allow-root
+
+wp config create --dbname==$DB_NAME --dbuser=$DB_USER --dbhost=mariadb:3306 --path=/var/www/html --allow-root
+
+wp core install --url=$DOMAIN_NAME --title="ykhalil" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --path=/var/www/html --allow-root
+
+wp user create $WP_USER $WP_EMAIL --role=editor --user_pass=$WP_PASS --path=/var/www/html --allow-root
+
+php-fpm8.2 -F
