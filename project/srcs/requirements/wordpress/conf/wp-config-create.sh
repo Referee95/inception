7eb65e4283ce8fd5b87566629b02c20b18a3eef7
referee95 <ysfm7khalil@gmail.com>
2024-07-14 12:14:24 -0400

    docker compose

--- project/srcs/requirements/wordpress/conf/wp-config-create.sh
+++ project/srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
+
+mkdir -p /run/php
+
 sed -i "s|listen = /run/php/php8.2-fpm.sock|listen = 0.0.0.0:9000|g" /etc/php/8.2/fpm/pool.d/www.conf
+
+chown -R 777 www-data:www-data /var/www/html 
+
+
+cd /var/www/html
 
 wp core download --path=/var/www/html --allow-root
 
-wp config create --dbname==$DB_NAME --dbuser=$DB_USER --dbhost=mariadb:3306 --path=/var/www/html --allow-root
+wp config create    --dbname=$DB_NAME \
+                    --dbuser=$DB_USER \
+                    --dbpass=$DB_PASS \
+                    --dbhost=mariadb:3306 \
+                    --path=/var/www/html \
+                    --allow-root
 
-wp core install --url=$DOMAIN_NAME --title="ykhalil" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --path=/var/www/html --allow-root
+wp core install  --url=$DOMAIN_NAME \
+                 --title="ykhalil" \
+                 --admin_user=$WP_ADMIN \
+                 --admin_password=$WP_ADMIN_PASS \
+                 --admin_email=$WP_ADMIN_EMAIL \
+                 --path=/var/www/html --allow-root --skip-email
 
 wp user create $WP_USER $WP_EMAIL --role=editor --user_pass=$WP_PASS --path=/var/www/html --allow-root
 
