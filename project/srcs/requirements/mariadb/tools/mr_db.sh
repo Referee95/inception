2cccbe63cb2eeff223858cf537276d95f9b592eb
referee95 <ysfm7khalil@gmail.com>
2024-07-18 10:27:05 -0400

    mochkil 3aabiit .

--- project/srcs/requirements/mariadb/tools/mr_db.sh
+++ project/srcs/requirements/mariadb/tools/mr_db.sh
@@ -2,11 +2,9 @@
 
 echo "\
 FLUSH PRIVILEGES;
-
-ALTER USER'root'@'localhost' IDENTIFIED BY '1234';
 CREATE DATABASE IF NOT EXISTS $DB_NAME ;
 CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
-GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
 
 FLUSH PRIVILEGES;" > /init_db.sql
 
@@ -14,3 +12,8 @@ mariadbd --user=mysql --bootstrap < /init_db.sql;
 
 exec "$@" 
 
+
+
+
+
+# ALTER USER'root'@'localhost' IDENTIFIED BY '$DB_PASS';
